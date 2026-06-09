adac1c51b5128ec6f9e00db7b1ce5967b9386823
vladimirmomiroski <83420111+vladimirmomiroski@users.noreply.github.com>
2026-06-09 13:20:37 +0200

    Add listing domain model

---
+++ src/RealEstate.Domain/Enums/ListingStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate.Domain.Enums;
+
+public enum ListingStatus
+{
+    Draft = 1,
+    Active = 2,
+    Reserved = 3,
+    Sold = 4,
+    Rented = 5,
+    Archived = 6
+}
---
+++ src/RealEstate.Domain/Enums/ListingType.cs
@@ -0,0 +1,9 @@
+﻿
+namespace RealEstate.Domain.Enums
+{
+    public enum ListingType
+    {
+        Sale = 1,
+        Rent = 2,
+    }
+}
